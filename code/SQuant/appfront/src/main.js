4621ee19b39223b2812802a0907566abc202ea0a
gccn <chengguohustsjtu@gmail.com>
2018-10-25 15:42:59 +0800

    fix main.js module

--- code/SQuant/appfront/src/main.js
+++ code/SQuant/appfront/src/main.js
@@ -5,7 +5,7 @@ import App from './App'
 import router from './router'
 import ElementUI from 'element-ui'
 import VueResource from 'vue-resource'
-import 'element-ui/lib/theme-default/index.css'
+import 'element-ui/lib/theme-chalk/index.css'
 
 Vue.config.productionTip = false
 
