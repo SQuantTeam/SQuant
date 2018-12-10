695b264b68909bf6b3f756b85653e144e68772bb
chantungyee <1450126@tongji.edu.cn>
2018-12-10 09:10:11 +0800

    头部组件化

--- code/SQuant/appfront/src/components/holdPosition.vue
+++ code/SQuant/appfront/src/components/holdPosition.vue
@@ -1,41 +1,6 @@
 <template>
     <div>
-        <el-row type="flex" class="row-bg" style="background-color:#252a2f;">
-            <el-col :span="3">
-                <!-- <div style="width:65px;height:65px;border:1px solid hsla(0,0%,100%,.15);background-image:url(../assets/logo-sq.png);background-repeat:no-repeat;background-position:center;background-size: 100% 100%;"></div> -->
-                <div id="nav_logo_collapsed"></div>
-            </el-col>
-
-            <el-col :span="12" :offset="15">
-                <el-menu :default-active="this.$router.path" class="el-menu-demo" mode="horizontal" text-color="#fff" active-text-color="#fff">
-                    <el-menu-item index='/#/'>
-                        <a href="./">SQuant</a>
-                    </el-menu-item>
-                    <el-menu-item index='/more'>
-                        <a href="/#/more">行情信息</a>
-                    </el-menu-item>
-                    <el-menu-item index='/holdPosition'>
-                        <a href="/#/holdPosition">持仓信息</a>
-                    </el-menu-item>
-                    <el-menu-item index="/#/" style="width:80px">
-                        <a href="./"><img src="../assets/usr.png" style="width:100%"></a>
-                    </el-menu-item>
-                </el-menu>
-            </el-col>
-        </el-row>
-
-        <el-menu id="vertical-banner" default-active="5" class="el-menu-vertical-demo" :collapse="isCollapse" style="position:absolute;height:90%;">
-            <el-menu-item index="5">
-                <i class="el-icon-menu"></i>
-            </el-menu-item>
-            <el-menu-item index="7">
-                <i class="el-icon-document"></i>
-            </el-menu-item>
-            <el-menu-item index="8">
-                <i class="el-icon-setting"></i>
-            </el-menu-item>
-        </el-menu>
-
+        <squantheader></squantheader>
         <el-col :span="23" :offset="1">
             <div class="ant-layout">
                 <div class="ant-layout-content">
@@ -285,31 +250,6 @@
 
 <style <style scoped>
 @import "../assets/css/tradeSimApp.style.css";
-
-body {
-  background: red;
-}
-
-#nav_logo_collapsed {
-  background-image: url(../assets/logo-sq.png);
-  background-position: center;
-  background-repeat: no-repeat;
-  background-size: 50%;
-  position: absolute;
-  width: 65px;
-  height: 60px;
-  border-bottom: 1px solid hsla(0, 0%, 100%, 0.15);
-}
-
-.el-menu-vertical-demo:not(.el-menu--collapse) {
-  width: 200px;
-  min-height: 400px;
-}
-
-#vertical-banner {
-  background-color: #252a2f;
-}
-
 #stock_details_part1 {
   width: 60%;
   border: 1px solid black;
@@ -335,7 +275,9 @@ body {
 import axios from 'axios'
 import '../global.js'
 // import Toast from 'toast/Toast.vue'
+import squantheader from '@/components/header'
 export default {
+    components: {squantheader},
     data() {
         return {
             activeIndex: "1",
--- code/SQuant/appfront/src/components/strategy.vue
+++ code/SQuant/appfront/src/components/strategy.vue
@@ -1,29 +1,6 @@
 <template>
     <div>
-        <el-row type="flex" class="row-bg" style="background-color:#252a2f;">
-            <el-col :span="3">
-                <!-- <div style="width:65px;height:65px;border:1px solid hsla(0,0%,100%,.15);background-image:url(../assets/logo-sq.png);background-repeat:no-repeat;background-position:center;background-size: 100% 100%;"></div> -->
-                <div id="nav_logo_collapsed"></div>
-            </el-col>
-
-            <el-col :span="12" :offset="15">
-                <el-menu :default-active="this.$router.path" class="el-menu-demo" mode="horizontal" text-color="#fff" active-text-color="#fff">
-                    <el-menu-item index='/#/'>
-                        <a href="./">SQuant</a>
-                    </el-menu-item>
-                    <el-menu-item index='/more'>
-                        <a href="/#/more">行情信息</a>
-                    </el-menu-item>
-                    <el-menu-item index='/holdPosition'>
-                        <a href="/#/holdPosition">持仓信息</a>
-                    </el-menu-item>
-                    <el-menu-item index="/#/" style="width:80px">
-                        <a href="./"><img src="../assets/usr.png" style="width:100%"></a>
-                    </el-menu-item>
-                </el-menu>
-            </el-col>
-        </el-row>
-
+        <squantheader></squantheader>
         <div  style="position:absolute;top:10%;left: 2%;">
             <el-card>
             <el-form ref="form" :model="alg_details" label-width="100px" size="mini">
@@ -176,23 +153,6 @@
 
 
 <style <style scoped>
-body {
-    background: red;
-    /* font-family: "Helvetica Neue",Helvetica,"PingFang SC","Hiragino Sans GB","Microsoft YaHei","\5FAE\8F6F\96C5\9ED1",Arial,sans-serif !important; */
-    /* font-size: 16px !important; */
-}
-
-#nav_logo_collapsed {
-    background-image:url(../assets/logo-sq.png);
-    background-position:center;
-    background-repeat:no-repeat;
-    background-size: 50%;
-    position: absolute;
-    width: 65px;
-    height: 60px;
-    border-bottom: 1px solid hsla(0,0%,100%,.15);
-}
-
 .el-form-item--mini.el-form-item, .el-form-item--small.el-form-item {
     margin-bottom: 1px !important;
 }
@@ -203,7 +163,9 @@ body {
 <script>
 import axios from 'axios'
 import echarts from 'echarts'
+import squantheader from '@/components/header'
 export default {
+    components: {squantheader},
     data() {
       return {
             alg_search: '',
