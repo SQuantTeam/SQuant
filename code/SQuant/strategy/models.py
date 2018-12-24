3f2b84fcca97befa043cab76f989ea8ef70de2fb
gccn <chengguohustsjtu@gmail.com>
2018-12-24 13:36:17 +0800

    第15周工作

---
+++ code/SQuant/strategy/models.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+
+class Algorithem(models.Model):
+    email = models.ForeignKey("market.User")
+    name = models.CharField(max_length=255, unique=True, default='')
+    file_path = models.CharField(max_length=255, default='')
+
+
+class Strategy(models.Model):
+    email = models.ForeignKey("market.User")
+    name = models.CharField(max_length=255, unique=True, default='')
+    file_path = models.CharField(max_length=255, default='')
