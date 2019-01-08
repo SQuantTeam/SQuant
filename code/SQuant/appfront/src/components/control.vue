d0097bc8bf902860d31b3051496c82120db3ef31
chantungyee <1450126@tongji.edu.cn>
2019-01-09 00:27:49 +0800

    fix bugs

--- code/SQuant/appfront/src/components/control.vue
+++ code/SQuant/appfront/src/components/control.vue
@@ -2,36 +2,24 @@
     <div>
         <squantheader></squantheader>
         <div >
-            <el-card :body-style="{ padding: '20px' }" style="width:30%;position:absolute;left:32%;top:13%;" > 
+            <el-card :body-style="{ padding: '20px' }" style="width:32%;position:absolute;left:32%;top:23%;" > 
             <h4 style="margin-bottom: 20px;margin-top:10px">风控模块</h4>
-            <el-form ref="form" :model="security_details"  label-width="110px" size="mini">
+            <el-form ref="form" :model="security_details"  label-width="160px" size="mini">
                 <el-form-item label="工作状态">
                     <el-button v-if="security_status" type="success" @click="toggle_status(0)" style="width:100%">风控模块运行中</el-button>
                     <el-button v-else type="info" @click="toggle_status(1)" style="width:100%">风控模块已停止</el-button>
                 </el-form-item>
-                <el-form-item label="流量上限">
-                    <el-input v-model="security_details.flow_ceil"></el-input>
+                <el-form-item label="当日成交股数上限">
+                    <el-input v-model="security_details.trade_limit"></el-input>
                 </el-form-item>
-                <el-form-item label="流量清空（秒）">
-                    <el-input v-model="security_details.flow_clear"></el-input>
+                <el-form-item label="单笔金额较行情比率">
+                    <el-input v-model="security_details.order_price_upper_limit"></el-input>
                 </el-form-item>
-                <el-form-item label="单笔委托上限">
-                    <el-input v-model="security_details.one_ceil"></el-input>
+                <el-form-item label="单笔股数上限">
+                    <el-input v-model="security_details.order_size_limit"></el-input>
                 </el-form-item>
-                <el-form-item label="总成交上限">
-                    <el-input v-model="security_details.all_ceil"></el-input>
-                </el-form-item>
-                <el-form-item label="活动订单上限">
-                    <el-input v-model="security_details.hand_ceil"></el-input>
-                </el-form-item>
-                <el-form-item label="单合约撤单上限">
-                    <el-input v-model="security_details.one_cancel_ceil"></el-input>
-                </el-form-item>
-                <el-form-item label="保证金占比上限">
-                    <el-input v-model="security_details.cash_ceil"></el-input>
-                </el-form-item>
-                <el-form-item>
-                    <el-button type="primary" @click="save_control" style="margin-right:-70%">保存设置</el-button>
+                <el-form-item label="单笔金额较可用金比率">
+                    <el-input v-model="security_details.balance_use_limit"></el-input>
                 </el-form-item>
             </el-form>
             </el-card>
@@ -72,24 +60,95 @@ export default {
         return {
             security_status: 0,
             security_details: {
-                flow_ceil: 51,
-                flow_clear: 1,
-                one_ceil: 100,
-                all_ceil: 1000,
-                hand_ceil: 20,
-                one_cancel_ceil: 10,
-                cash_ceil: '85%'
+                trade_limit: 100,
+                order_size_limit: 100,
+                order_price_upper_limit: 1.0,
+                balance_use_limit: 10000,
             },
         };
     },
 
     methods: {
         toggle_status(new_status){
-            this.security_status = new_status;
+            if(new_status == 0){
+                this.stop_control();
+            } else{
+                this.run_control();
+            };
         },
-        save_control(){
+        is_valid_input(){
+            var json = {
+                trade_limit: parseInt(this.security_details.trade_limit),
+                order_size_limit: parseInt(this.security_details.order_size_limit),
+                order_price_upper_limit: parseFloat(this.security_details.order_price_upper_limit),
+                balance_use_limit: parseFloat(this.security_details.balance_use_limit),
+            };
+            
+            if(isNaN(json.trade_limit) || isNaN(json.order_size_limit) || isNaN(json.order_price_upper_limit) || isNaN(json.balance_use_limit)){
+                return false;
+            } else if((json.trade_limit==null) || (json.order_size_limit==null) || (json.order_price_upper_limit==null) || (json.balance_use_limit==null)){
+                return false;
+            }
+            this.security_details = json;
+            return true;
+        },
+        run_control(){
+            var self = this;
+            if(this.is_valid_input()==false){
+                this.$message({
+                    message: "请输入非空数值",
+                    type: 'error'
+                });
+                return;
+            }
+            var json = {
+                trade_limit: parseInt(this.security_details.trade_limit),
+                order_size_limit: parseInt(this.security_details.order_size_limit),
+                order_price_upper_limit: parseFloat(this.security_details.order_price_upper_limit),
+                balance_use_limit: parseFloat(this.security_details.balance_use_limit),
+            };
+            this.$axios.post("http://localhost:8000/squant/market/activateRiskManager", json).then(function(response) {
+                if(response.data.error_num==0) {
+                    self.security_status=1;
+                    self.$message({
+                        message: response.data.msg,
+                        type: 'success'
+                    });
+                    self.security_status = 1;
+                }
+            });
+        },
+        stop_control(){
+            var self = this;
+            this.$axios.get("http://localhost:8000/squant/market/closeRiskManager").then(function(response) {
+                if(response.data.error_num==0) {
+                    self.security_status=0;
+                    self.$message({
+                        message: response.data.msg,
+                        type: 'success'
+                    });
+                    self.security_status = 0;
+                }
+            });
+        },
+        get_risk_control(){
+            var self = this;
+            this.$axios.get("http://localhost:8000/squant/market/getRiskManagerStatus").then(function(response) {
+                if(response.data.error_num==0) {
+                    if(response.data.active==true){
+                        self.security_status=1;
+                        self.security_details.trade_limit=response.data.trade_limit;
+                        self.security_details.order_size_limit=response.data.order_size_limit;
+                        self.security_details.order_price_upper_limit=response.data.order_price_upper_limit;
+                        self.security_details.balance_use_limit=response.data.balance_use_limit;
+                    }
+                }
+            });
 
-        }
-    }
+        },
+    },
+    mounted() {
+        this.get_risk_control();
+    },
 }
 </script>
