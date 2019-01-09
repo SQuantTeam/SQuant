98f6b0d8b51780b86c4ec74075a55ab4a6a7d202
OptimusPrime1997 <874855266@qq.com>
2019-01-09 19:09:24 +0800

    Fix:modify bugs

--- code/SQuant/reinforce/models.py
+++ code/SQuant/reinforce/models.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.db import models
 import datetime
+import json
 
 
 # Create your models here.
@@ -15,3 +16,12 @@ class Learning(models.Model):
     date = models.CharField(max_length=10, default=str(datetime.date.today()), )
     prefer = models.SmallIntegerField(default=1)
     running = models.SmallIntegerField(default=0)
+
+    def toJSON(self):
+        fields = []
+        for field in self._meta.fields:
+            fields.append(field.name)
+        d = {}
+        for attr in fields:
+            d[attr] = getattr(self, attr)
+        return json.dumps(d)
--- code/SQuant/reinforce/urls.py
+++ code/SQuant/reinforce/urls.py
@@ -9,6 +9,6 @@ urlpatterns = [
     url(r'barData/(?P<symbol>[0-9]{6}\.S[HZ])/(?P<prefer>[123])$', views.getBarData, ),
     url(r'learn/add', views.add_learning, ),
     url(r'learn/(?P<email>([A-Za-z0-9_\-\.\u4e00-\u9fa5])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,8})$)',
-        views.add_learning, ),
+        views.get_learning, ),
     url(r'learn/update', views.update_learning, ),
 ]
